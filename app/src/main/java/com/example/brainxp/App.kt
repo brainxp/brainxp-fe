2f859aaaaf5169a356cc452b1b1348def0c2cc29
wildanzhafiri <wildanzhaf@gmail.com>
2026-09-04 09:46:25 +0700

    feat: register permission resume observer in application

--- app/src/main/java/com/example/brainxp/App.kt
+++ app/src/main/java/com/example/brainxp/App.kt
@@ -1,7 +1,18 @@
 package com.example.brainxp
 
 import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.example.brainxp.core.permission.PermissionResumeObserver
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    @Inject
+    lateinit var permissionResumeObserver: PermissionResumeObserver
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(permissionResumeObserver)
+    }
+}
