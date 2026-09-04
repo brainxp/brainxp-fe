35cdc4c65ef2c178e5fa749f86808996bcafa409
wildanzhafiri <wildanzhaf@gmail.com>
2026-09-04 08:24:53 +0700

    feat: add colour ramp tokens for the design system

---
+++ app/src/main/java/com/example/brainxp/core/ui/Tokens.kt
@@ -0,0 +1,35 @@
+package com.example.brainxp.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+internal object Tokens {
+    val Blue900 = Color(0xFF08152F)
+    val Blue800 = Color(0xFF0C2350)
+    val Blue700 = Color(0xFF123A85)
+    val Blue600 = Color(0xFF1A56C4)
+    val Blue500 = Color(0xFF2F6FE4)
+    val Blue300 = Color(0xFF9DBEF8)
+    val Blue100 = Color(0xFFDDE8FD)
+    val Blue50 = Color(0xFFF2F6FE)
+
+    val Neutral900 = Color(0xFF0C1220)
+    val Neutral800 = Color(0xFF1B2436)
+    val Neutral700 = Color(0xFF39435A)
+    val Neutral500 = Color(0xFF6B7488)
+    val Neutral400 = Color(0xFF98A0B2)
+    val Neutral300 = Color(0xFFC8CFDC)
+    val Neutral200 = Color(0xFFE5E9F1)
+    val Neutral100 = Color(0xFFF0F3F8)
+    val Neutral0 = Color(0xFFFFFFFF)
+
+    val Alert = Color(0xFFC8452E)
+    val AlertSurface = Color(0xFFFBEAE6)
+    val AlertBorder = Color(0xFFF3D2CB)
+    val AlertInk = Color(0xFF7A2617)
+
+    val Ok = Color(0xFF1D7A55)
+    val OkSurface = Color(0xFFEFF7F3)
+    val OkInk = Color(0xFF16543C)
+
+    val Page = Color(0xFFEEF1F7)
+}
