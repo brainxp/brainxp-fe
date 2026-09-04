9b5c43ea7c03c69a0a8692b0edac5d4fa2c16c98
wildanzhafiri <wildanzhaf@gmail.com>
2026-09-04 17:12:02 +0700

    feat: add session start events and a spend failure effect

--- app/src/main/java/com/example/brainxp/feature/home/HomeEvent.kt
+++ app/src/main/java/com/example/brainxp/feature/home/HomeEvent.kt
@@ -1,11 +1,19 @@
 package com.example.brainxp.feature.home
 
+import com.example.brainxp.core.result.ApiError
+
 sealed interface HomeEvent {
     data object Retry : HomeEvent
 
     data object StartEarning : HomeEvent
 
     data object EndUnlockEarly : HomeEvent
+
+    data object StartSession : HomeEvent
+
+    data class SelectDuration(
+        val seconds: Int,
+    ) : HomeEvent
 
     data object FixPermissions : HomeEvent
 
@@ -21,6 +29,10 @@ sealed interface HomeEvent {
 }
 
 sealed interface HomeEffect {
+    data class SpendFailed(
+        val error: ApiError,
+    ) : HomeEffect
+
     data object OpenAddMaterial : HomeEffect
 
     data object OpenPermissionSetup : HomeEffect
