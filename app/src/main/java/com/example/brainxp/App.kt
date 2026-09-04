e8c35545196a2286795a4c8699b31514c866ad80
wildanzhafiri <wildanzhaf@gmail.com>
2026-09-04 13:44:34 +0700

    feat: start the protection controller on app create

--- app/src/main/java/com/example/brainxp/App.kt
+++ app/src/main/java/com/example/brainxp/App.kt
@@ -2,6 +2,7 @@ package com.example.brainxp
 
 import android.app.Application
 import androidx.lifecycle.ProcessLifecycleOwner
+import com.example.brainxp.blocking.ProtectionController
 import com.example.brainxp.core.permission.PermissionResumeObserver
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -11,8 +12,12 @@ class App : Application() {
     @Inject
     lateinit var permissionResumeObserver: PermissionResumeObserver
 
+    @Inject
+    lateinit var protectionController: ProtectionController
+
     override fun onCreate() {
         super.onCreate()
         ProcessLifecycleOwner.get().lifecycle.addObserver(permissionResumeObserver)
+        protectionController.start()
     }
 }
