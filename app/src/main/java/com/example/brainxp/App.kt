b94b06b302aa01e4b7827fae2aa4fb561cb77290
wildanzhafiri <wildanzhaf@gmail.com>
2026-09-05 13:43:31 +0700

    chore: provide the hilt worker factory to work manager

--- app/src/main/java/com/example/brainxp/App.kt
+++ app/src/main/java/com/example/brainxp/App.kt
@@ -1,19 +1,29 @@
 package com.example.brainxp
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.work.Configuration
 import com.example.brainxp.blocking.ProtectionController
 import com.example.brainxp.core.permission.PermissionResumeObserver
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application() {
+class App :
+    Application(),
+    Configuration.Provider {
     @Inject
     lateinit var permissionResumeObserver: PermissionResumeObserver
 
     @Inject
     lateinit var protectionController: ProtectionController
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder().setWorkerFactory(workerFactory).build()
 
     override fun onCreate() {
         super.onCreate()
